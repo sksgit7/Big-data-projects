1ae8e6e207dd075e4e7d27b0957a3ed88c1fd0ed
Subham Kumar Sahoo <56225983+sksgit7@users.noreply.github.com>
2022-10-07 19:59:18 +0530

    Update db_writer1.py

--- kafka project1/db_writer1.py
+++ kafka project1/db_writer1.py
@@ -86,7 +86,7 @@ def main(topic):
                 #time.sleep(3)
                 ##
                 cnx = conn.connect(host = "localhost", user = "root",
-                    passwd = "mysql", database = "test")
+                    passwd = "your-password", database = "test")
                 cur = cnx.cursor()
                 query = "insert into bid (name, price, bid_ts) values ( %s, %s, %s)"
                 data = (name,price,bid_ts)
